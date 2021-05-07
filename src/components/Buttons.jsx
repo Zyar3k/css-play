cda3d765d9b29c94876473f2c20da3cafb31e73a
Arkadiusz Zygan <a.zygan@gmail.com>
2021-05-07 14:43:12 +0200

    Prepare components to styling

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,7 +1,51 @@
 const Buttons = () => {
   return (
-    <main className='container'>
-      <h1>buttons</h1>
+    <main className='buttons container'>
+      <div className='btnContainer'>
+        <button className='btn1'>Button</button>
+      </div>
+      <div className='btnContainer'>
+        <button className='btn2'>Button</button>
+      </div>
+      <div className='btnContainer'>
+        <button className='btn3'>Button</button>
+      </div>
+      <div className='btnContainer'>
+        <button className='btn4'>Button</button>
+      </div>
+      <div className='btnContainer'>
+        <button className='btn5'>Button</button>
+      </div>
+      <div className='btnContainer'>
+        <button className='btn6'>Button</button>
+      </div>
+      <div className='btnContainer'>
+        <button className='btn7'>Button</button>
+      </div>
+      <div className='btnContainer'>
+        <button className='btn8'>Button</button>
+      </div>
+      <div className='btnContainer'>
+        <button className='btn9'>Button</button>
+      </div>
+      <div className='btnContainer'>
+        <button className='btn10'>Button</button>
+      </div>
+      <div className='btnContainer'>
+        <button className='btn11'>Button</button>
+      </div>
+      <div className='btnContainer'>
+        <button className='btn12'>Button</button>
+      </div>
+      <div className='btnContainer'>
+        <button className='btn13'>Button</button>
+      </div>
+      <div className='btnContainer'>
+        <button className='btn14'>Button</button>
+      </div>
+      <div className='btnContainer'>
+        <button className='btn15'>Button</button>
+      </div>
     </main>
   );
 };
--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,7 +1,54 @@
 const Inputs = () => {
   return (
-    <main className='container'>
-      <h1>Inputs</h1>
+    <main className='inputs container'>
+      <div className='inputContainer'>
+        <label htmlFor=''>
+          Text:
+          <input type='text' />
+        </label>
+      </div>
+      <div className='inputContainer'>
+        <label htmlFor=''>
+          Button:
+          <input type='button' value='button' />
+        </label>
+      </div>
+      <div className='inputContainer'>
+        <label htmlFor=''>
+          Date:
+          <input type='date' name='' id='' />
+        </label>
+      </div>
+      <div className='inputContainer'>
+        <label htmlFor=''>
+          Email:
+          <input type='email' name='' id='' />
+        </label>
+      </div>
+      <div className='inputContainer'>
+        <label htmlFor=''>
+          Password:
+          <input type='password' name='' id='' />
+        </label>
+      </div>
+      <div className='inputContainer'>
+        <label htmlFor=''>
+          Range:
+          <input type='range' name='' id='' />
+        </label>
+      </div>
+      <div className='inputContainer'>
+        <label htmlFor=''>
+          Search:
+          <input type='search' name='' id='' />
+        </label>
+      </div>
+      <div className='inputContainer'>
+        <label htmlFor=''>
+          Time:
+          <input type='time' name='' id='' />
+        </label>
+      </div>
     </main>
   );
 };
